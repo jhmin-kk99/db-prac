f36b98f524e6d4d4bd6ef7081c235646a89c60dc
jihong <jihongmin99@naver.com>
2024-01-16 20:42:06 +0900

    understanding the sql query planner and optimizer with explain

--- grades.sql
+++ grades.sql
@@ -3,8 +3,6 @@
 -- make sure to run the container with at least 1gb shared memory
 -- docker run --name pg —shm-size=1g -e POSTGRES_PASSWORD=postgres —name pg postgres
 
-
-
 create table grades (
 id serial primary key, 
  g int,
@@ -21,5 +19,39 @@ substring(md5(random()::text ),0,floor(random()*31)::int)
 
 vacuum (analyze, verbose, full);
 
+create index g_idx on grades(g);
+
 explain analyze select id,g from grades where g > 80 and g < 95 order by g;
 
+explain select * from grades; 
+
+/** 
+QUERY PLAN ... Seq Scan on grades cost a..b 
+(a : first page를 가져오는 데 걸린 시간, 이 값이 커지면,you're doing a lot of stuff before fetching.
+b : essentially the total amount of time that it thinks (not really excution time)))
+
+rows : not an accurate number, but it gives you a quick number, approximate based on its
+own statistics.
+
+Don't do select count. SELECT COUNT will kill your performance. it's actually and physically
+do the count on all your rows eventhough you have billion rows.
+
+width: width of the row, this is sum of all the bytes for all the columns.
+**/
+
+explain select * from grades order by g; 
+/** cost의 첫 숫자가 좀 늘었음 -> postgres did some work or it is attempting to do some work before 
+fetching the row. and that work is actually doing the sort order by g, and it's not so bad
+because it did use the index that is on g to do the sorting. **/
+
+explain select * from grades order by name;
+/** 오래 걸림 name이 indexing 안 되어있음, 다 찾고 sort해야 함 **/
+
+explain select id from grades;
+/** width : 4 bytes **/
+explain select name from grades;
+/** width = 19 ;  바이트 단위로 표시, the larger the network you're going to take,
+the higher the TCP packets. don't put like a blob and do select star 
+필요한만큼만 select 하셈 **/
+
+explain select * from grades where id = 10;
